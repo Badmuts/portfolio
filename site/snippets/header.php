efe9c2839ab62b70a106b883895c5d6655ef2419
Daan Rosbergen <d.rosbergen@gmail.com>
2014-12-28 16:21:17 +0100

    Add snippets

---
+++ site/snippets/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <base href="<?= kirby()->urls()->index() ?>/"></base>
+    <meta charset="UTF-8">
+    <meta name="description" content="<?= $site->description() ?>">
+    <meta name="keywords" content="<?= $site->keywords() ?>">
+    <meta name="author" content="Daan Rosbergen">
+    <meta name="theme-color" content="#27303d">
+    <title><?= $page->title() ?> - <?= $site->title() ?></title>
+    <link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="assets/components/animate.css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/grid.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class="">
+    <header class="header" id="header">
+        <h1 class="letters rotate-2">
+            <span class="light">I am a</span>
+            <span class="word-wrapper">
+                <b class="is-visible">webdesigner</b>
+                <b>webdeveloper</b>
+            </span>
+        </h1>
+    </header>
---
+++ site/snippets/menu.php
@@ -0,0 +1,11 @@
+<nav>
+    <div class="container">
+        <div class="logo">
+            Daan <span class="light">Rosbergen</span>
+        </div>
+        <ul>
+            <li class="active"><a href="#work">Work</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </div>
+</nav>
