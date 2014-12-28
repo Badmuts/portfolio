b4ac88f8d7413c6b557adf4c0ba5dc4c9a4b25db
Daan Rosbergen <d.rosbergen@gmail.com>
2014-12-28 12:47:23 +0100

    Setup Kirby panel

---
+++ site/accounts/daan.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: daan
+email: d.rosbergen@gmail.com
+password: >
+  $2a$10$Z4BxoOBUuqjBQt9ZIZPX.uMZ/fQK/KglonnPrdj9evMOE1mTIlL1y
+language: en
+role: admin
+token: f6b02c6cc252ca93ef77ee2ccf84f4f10dae3cc3
+history: [ ]
