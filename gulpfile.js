c331e5b3e661fdcd955941070fa49a26c493d57e
Daan Rosbergen <d.rosbergen@gmail.com>
2015-07-09 17:27:43 +0200

    Add karma to project. Setup first jasmine test

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var karma = require('karma').server;
 
 gulp.task('sass', function () {
     gulp.src('./assets/css/sass/*.scss')
-	.pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./assets/css'));
 });
 
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/tests/karma.conf.js',
+    singleRun: true
+  }, done);
+});
+
 gulp.task('watch', function () {
 	gulp.watch("./assets/css/sass/*.scss", ['sass']);
+    gulp.watch("./assets/js/*.js", ['test']);
 });
